80a45769168784f93ac78a3ad84aacd0e32cde42
anttagjan <38495085+anttagjan@users.noreply.github.com>
2019-06-24 16:53:05 +0200

    Update IOPlyLimeSeg.java
    implement search files and load a .ply file

--- PostProcessingGland/src/main/java/PostProcessingGland/IOPlyLimeSeg.java
+++ PostProcessingGland/src/main/java/PostProcessingGland/IOPlyLimeSeg.java
@@ -1,3 +1,4 @@
+
 package PostProcessingGland;
 
 import java.io.File;
@@ -8,6 +9,8 @@ import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import javax.xml.parsers.DocumentBuilderFactory;
 
 import org.smurn.jply.ElementReader;
 import org.smurn.jply.ElementType;
@@ -21,9 +24,7 @@ import eu.kiaru.limeseg.struct.DotN;
  * Class that will be called by Fiji/ImageJ and load the output of the 3D
  * Segmentation Adaptation of LimeSeg (Lipid Membrane Segmentation): a
  * coarse-grained lipid membrane simulation for 3D image segmentation - by Sarah
- * Machado et al. 
- * 
- * A computationally efficient and spatially continuous 3D
+ * Machado et al. A computationally efficient and spatially continuous 3D
  * segmentation method. LimeSeg is easy-to-use and can process many and/or
  * highly convoluted objects. Based on the concept of SURFace ELements
  * (“Surfels”), LimeSeg resembles a highly coarse-grained simulation of a lipid
@@ -37,120 +38,154 @@ import eu.kiaru.limeseg.struct.DotN;
  */
 
 public class IOPlyLimeSeg {
+	
+	private ArrayList<DotN> dots = new ArrayList<DotN>();
 
 	public IOPlyLimeSeg() {
 		// TODO Auto-generated constructor stub
 	}
 
-static public void loadCellTFromPly(String path) {
-  try {		   
-	   PlyReader plyreader = new PlyReaderFile(path);
-	   ElementReader reader = plyreader.nextElementReader();
-      while (reader != null) {
-           ElementType type = reader.getElementType();
-           // In PLY files vertices always have a type named "vertex".
-           if (type.getName().equals("vertex")) {
-               ArrayList<DotN> dots = new ArrayList<DotN>(reader.getCount());
-               // Read the elements. They all share the same type.
-           	org.smurn.jply.Element element = reader.readElement();
-               while (element != null) {
-               	 DotN dn = new DotN();
-               	 dn.pos.x=(float) element.getDouble("x");
-               	 dn.pos.y=(float) element.getDouble("y");
-               	 dn.pos.z=(float) element.getDouble("z");
+	static public void loadPlyFile(ArrayList<DotN> dots, String path) {
+		try {
+			PlyReader plyreader = new PlyReaderFile(path);
+			ElementReader reader = plyreader.nextElementReader();
+			while (reader != null) {
+				ElementType type = reader.getElementType();
+				// In PLY files vertices always have a type named "vertex".
+				if (type.getName().equals("vertex")) {
+					// dots = new ArrayList<DotN>(reader.getCount());
+					
+					// Read the elements. They all share the same type.
+					org.smurn.jply.Element element = reader.readElement();
+					while (element != null) {
+						DotN dn = new DotN();
+						dn.pos.x = (float) element.getDouble("x");
+						dn.pos.y = (float) element.getDouble("y");
+						dn.pos.z = (float) element.getDouble("z");
 
-               	 dn.Norm.x=(float) element.getDouble("nx");
-               	 dn.Norm.y=(float) element.getDouble("ny");
-               	 dn.Norm.z=(float) element.getDouble("nz");
-               	 //dn.ct=ct;
-                    element = reader.readElement();
-                    dots.add(dn);
-               }
-               //ct.dots=dots;
-           }
-          /* if (type.getName().equals("face")) {
-           	ArrayList<TriangleN> tris = new ArrayList<TriangleN>(reader.getCount());
-	            org.smurn.jply.Element triangle = reader.readElement();
-               while (triangle != null) {
-	            	int[] indices = triangle.getIntList("vertex_index");
-	            	TriangleN tri = new TriangleN();
-	            	tri.id1=indices[0];
-	            	tri.id2=indices[1];
-	            	tri.id3=indices[2];
-	            	triangle = reader.readElement();
-                   tris.add(tri);
-                   
-               }
-           	ct.triangles=tris;
-           	ct.tesselated=true;
-           }	*/            		            
-           // Close the reader for the current type before getting the next one.
-           reader.close();
-           reader = plyreader.nextElementReader();
-      }
-      plyreader.close();
-  } catch (IOException e) {
-		e.printStackTrace();
-  }
-}
+						dn.Norm.x = (float) element.getDouble("nx");
+						dn.Norm.y = (float) element.getDouble("ny");
+						dn.Norm.z = (float) element.getDouble("nz");
+						// dn.ct=ct;
+						element = reader.readElement();
+						dots.add(dn);
+					}
+					// ct.dots=dots;
+				}
+				/* if (type.getName().equals("face")) {
+				 	ArrayList<TriangleN> tris = new ArrayList<TriangleN>(reader.getCount());
+				    org.smurn.jply.Element triangle = reader.readElement();
+				     while (triangle != null) {
+				    	int[] indices = triangle.getIntList("vertex_index");
+				    	TriangleN tri = new TriangleN();
+				    	tri.id1=indices[0];
+				    	tri.id2=indices[1];
+				    	tri.id3=indices[2];
+				    	triangle = reader.readElement();
+				         tris.add(tri);
+				         
+				     }
+				 	ct.triangles=tris;
+				 	ct.tesselated=true;
+				 }	*/
+				// Close the reader for the current type before getting the next one.
+				reader.close();
+				reader = plyreader.nextElementReader();
+			}
+			plyreader.close();
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
-static public void hydrateCellT(Cell c, String path) {
-  // path should contain the folder with the ply files for each timepoint
-  c.cellTs.clear();
-  Pattern pattern = Pattern.compile("[0-9]+"); 
-  File dir = new File(path);
-  if (!dir.isDirectory()) {
- 		System.out.println("Error, folder for cell "+c.id_Cell+" not found.");
- 		return;
-  }		   
-  File[] files = dir.listFiles(new FilenameFilter() {
-       @Override
-       public boolean accept(File dir, String name) {
-           return name.matches("T_[0-9]+.ply");
-       }
-  });
-  for (File f:files) {
-	   String fileName = f.getAbsolutePath();
-	   System.out.println("Found cellT file : "+fileName);
-	   Matcher matcher = pattern.matcher(f.getName());
-	   matcher.find(); 			   
-	   String match = matcher.group(); // Get the matching string
-	   int tp = Integer.valueOf(match);
-	   System.out.println("TP="+tp);			   		   
-	   CellT ct = new CellT(c,tp);
-	   loadCellTFromPly(f.getAbsolutePath());
-	   ct.frame=tp;
-	   c.cellTs.add(ct);
-  }
-}
+	static public void SearchPath(ArrayList<DotN> dots, String path) {
+		// path should contain the folder with the ply files for each timepoint
+		Pattern pattern = Pattern.compile("[0-9]+");
+		File dir = new File(path);
+		if (!dir.isDirectory()) {
+			System.out.println("Error, folder for cell " + " not found.");
+			return;
+		}
+		File[] files = dir.listFiles(new FilenameFilter() {
 
-static public boolean askYesNo(String question) {
-     return askYesNo(question, "[Y]", "[N]");
-}
+			@Override
+			public boolean accept(File dir, String name) {
+				return name.matches("T_[0-9]+.ply");
+			}
+		});
+		dots.clear();
+		for (File f : files) {
+			String fileName = f.getAbsolutePath();
+			System.out.println("Found cellT file : " + fileName);
+			Matcher matcher = pattern.matcher(f.getName());
+			matcher.find();
+			String match = matcher.group(); // Get the matching string
+			int tp = Integer.valueOf(match);
+			System.out.println("TP=" + tp);
+			
+			loadPlyFile(dots, f.getAbsolutePath());
+		}
+	}
 
-static public boolean askYesNo(String question, String positive, String negative) {
-     Scanner input = new Scanner(System.in);
-     // Convert everything to upper case for simplicity...
-     positive = positive.toUpperCase();
-     negative = negative.toUpperCase();
-     String answer;
-     do {
-         System.out.print(question);
-         answer = input.next().trim().toUpperCase();
-     } while (!answer.matches(positive) && !answer.matches(negative));
-     // Assess if we match a positive response
-     input.close();
-     return answer.matches(positive);
-}
- 
-static void purgeDirectory(File dir, int height) {
-  // no need to clean below level 
-  if (height>=0) {
-   for (File file: dir.listFiles()) {
-       if (file.isDirectory()) purgeDirectory(file, height-1);
-       file.delete();
-   }
-  }
-}
+	/* static public boolean askYesNo(String question) {
+		return askYesNo(question, "[Y]", "[N]");
+	}
+
+	static public boolean askYesNo(String question, String positive,
+		String negative)
+	{
+		Scanner input = new Scanner(System.in);
+		// Convert everything to upper case for simplicity...
+		positive = positive.toUpperCase();
+		negative = negative.toUpperCase();
+		String answer;
+		do {
+			System.out.print(question);
+			answer = input.next().trim().toUpperCase();
+		}
+		while (!answer.matches(positive) && !answer.matches(negative));
+		// Assess if we match a positive response
+		input.close();
+		return answer.matches(positive);
+	}
+
+	static void purgeDirectory(File dir, int height) {
+		// no need to clean below level
+		if (height >= 0) {
+			for (File file : dir.listFiles()) {
+				if (file.isDirectory()) purgeDirectory(file, height - 1);
+				file.delete();
+			}
+		}
+	}
+
+	static public void loadState(LimeSeg lms, String path) {
+		if (!path.endsWith(File.separator)) {
+			path = path + File.separator;
+		}
+		try {
+			if (lms.exists()) {
+				lms.allCells.clear();
+				Cell c = new Cell();
+				lms.allCells.add(c);
+				c.id_Cell = cellName;
+				String pathToCell = path + cellName;
+				hydrateCellT(c, pathToCell);
+				File[] directories = new File(path).listFiles(File::isDirectory);
+				for (File currentPath : directories)
+					c.id_Cell = paramsCell.getParentFile().getName();
+				hydrateCellT(c, currentPath.getAbsolutePath());
+			}
+			else {
+				System.out.println(
+					"Cannot open file. Root node is not LimeSegParameters as expected");
+			}
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	 */
 
 }
