7a63a85e4c5da779a9f5dc22d881f365285d0b59
anttagjan <38495085+anttagjan@users.noreply.github.com>
2019-06-26 16:27:15 +0200

    Update PolygonalRoi.java

--- PostProcessingGland/src/main/java/PostProcessingGland/Elements/PolygonalRoi.java
+++ PostProcessingGland/src/main/java/PostProcessingGland/Elements/PolygonalRoi.java
@@ -1,10 +1,43 @@
 
 package PostProcessingGland.Elements;
 
-public class PolygonalRoi {
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Random;
 
-	public void convertDotsInPolygons() {
-		
+import ij.gui.PointRoi;
+import ij.gui.PolygonRoi;
+import eu.kiaru.limeseg.struct.DotN;
+
+public class PolygonalRoi {
+	
+	PointRoi dotsRoi;
+	PolygonRoi polyRoi;
+
+	public PointRoi convertPolygonInPointRois(PolygonRoi polyRoi) {
+		PointRoi dotsRoi = new PointRoi();
+		return dotsRoi;
+	}
+	
+	public void convertDotsInPointRoi(ArrayList<DotN> dots, int frame) {
+		Iterator<DotN> i=dots.iterator();
+         while (i.hasNext()) {
+         		DotN loadedDots = i.next();
+            int zpos=1+(int)(loadedDots.pos.z/ (float) 4.06); // zScale == 4.06
+            if (zpos == frame) {
+         		dotsRoi = new PointRoi(loadedDots.pos.x,loadedDots.pos.y);//,c); 
+             Random rand = new Random();
+             float r = rand.nextFloat();
+             float g = rand.nextFloat();
+             float b = rand.nextFloat();
+             Color randomColor = new Color(r, g, b);
+             //Color color = new Color((int)(loadedDots.ct.c.color[0]*255),(int)(loadedDots.ct.c.color[1]*255),(int)(loadedDots.ct.c.color[2]*255));
+             dotsRoi.setColor(randomColor);
+             //float zScale = workingImP  /workingImP.getHeight()
+             dotsRoi.setPosition(zpos);
+             }
+         }
 	}
 
 }
