400f942b331cebaaeb5a941af78037082e77910e
Pablo Vicente Munuera <pvicente1@us.es>
2019-08-07 15:26:42 +0200

    Create MainWindow.java
    New mainWindow

---
+++ AutomaticSeeds/src/main/java/AutomaticSegmentation/gui/MainWindow.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+package AutomaticSegmentation.gui;
+
+import java.awt.GraphicsConfiguration;
+import java.awt.HeadlessException;
+
+import javax.swing.JFrame;
+
+/**
+ * @author Pablo Vicente-Munuera, Pedro Gómez-Gálvez
+ *
+ */
+public class MainWindow extends JFrame {
+
+	/**
+	 * @throws HeadlessException
+	 */
+	public MainWindow() throws HeadlessException {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param gc
+	 */
+	public MainWindow(GraphicsConfiguration gc) {
+		super(gc);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param title
+	 * @throws HeadlessException
+	 */
+	public MainWindow(String title) throws HeadlessException {
+		super(title);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param title
+	 * @param gc
+	 */
+	public MainWindow(String title, GraphicsConfiguration gc) {
+		super(title, gc);
+		// TODO Auto-generated constructor stub
+	}
+
+}
