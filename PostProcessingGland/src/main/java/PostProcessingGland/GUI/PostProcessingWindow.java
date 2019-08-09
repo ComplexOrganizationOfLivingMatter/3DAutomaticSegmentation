444b3197d02b369e04b79167f1fb726a361b7d96
anttagjan <antonio.tagua@hotmail.es>
2019-08-09 16:10:44 +0200

    Format
    Co-Authored-By: Pablo Vicente Munuera <pvicente1@us.es>

--- PostProcessingGland/src/main/java/PostProcessingGland/GUI/PostProcessingWindow.java
+++ PostProcessingGland/src/main/java/PostProcessingGland/GUI/PostProcessingWindow.java
@@ -1,7 +1,6 @@
 
 package PostProcessingGland.GUI;
 
-
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Rectangle;
@@ -49,9 +48,7 @@ import ij.gui.Roi;
 import ij.gui.Overlay;
 import net.miginfocom.swing.MigLayout;
 
-public class PostProcessingWindow extends ImageWindow implements
-	ActionListener
-{
+public class PostProcessingWindow extends ImageWindow implements ActionListener {
 
 	private IOXmlPlyLimeSeg OutputLimeSeg;
 	private Hashtable<Integer, ArrayList<Roi>> cellsROIs;
@@ -62,7 +59,7 @@ public class PostProcessingWindow extends ImageWindow implements
 	public ArrayList<Cell> allCells;
 	public RoiAdjustment newCell;
 	public PolygonRoi polyRoi2;
-	
+
 	private JFrame processingFrame;
 	private JPanel upRightPanel;
 	private JPanel middlePanel;
@@ -102,8 +99,7 @@ public class PostProcessingWindow extends ImageWindow implements
 			String path = f.toString();
 			LimeSegCell.id_Cell = path.substring(path.indexOf("_") + 1);
 			OutputLimeSeg.hydrateCellT(LimeSegCell, path);
-			PostProcessCell = new Cell3D(LimeSegCell.id_Cell, LimeSegCell.cellTs.get(
-				0).dots);
+			PostProcessCell = new Cell3D(LimeSegCell.id_Cell, LimeSegCell.cellTs.get(0).dots);
 			PostProcessCell.labelCell = Integer.parseInt(LimeSegCell.id_Cell);
 			all3dCells.add(PostProcessCell);
 		}
@@ -118,20 +114,18 @@ public class PostProcessingWindow extends ImageWindow implements
 
 		canvas = (CustomCanvas) super.getCanvas();
 		PostProcessingGland.callToolbarPolygon();
-		
-		sliceSelector = new Scrollbar(Scrollbar.HORIZONTAL, 1,1,1,(imp.getStackSize()+1));
-	    sliceSelector.setVisible(true);
+
+		sliceSelector = new Scrollbar(Scrollbar.HORIZONTAL, 1, 1, 1, (imp.getStackSize() + 1));
+		sliceSelector.setVisible(true);
 
 		initializeGUIItems(raw_img);
 
-		
 		overlayResult = new ImageOverlay();
 		if (overlayResult != null) {
 
 			if (canvas.getImageOverlay() == null) {
 				canvas.clearOverlay();
-				raw_img.setOverlay(getOverlay(0, canvas.getImage()
-					.getCurrentSlice(), all3dCells, raw_img, false));
+				raw_img.setOverlay(getOverlay(0, canvas.getImage().getCurrentSlice(), all3dCells, raw_img, false));
 				canvas.addOverlay(overlayResult);
 				canvas.setImageOverlay(overlayResult);
 
@@ -155,28 +149,27 @@ public class PostProcessingWindow extends ImageWindow implements
 		middlePanel.add(btnSave, "wrap");
 
 		bottomRightPanel.setLayout(new MigLayout());
-		bottomRightPanel.setBorder(BorderFactory.createTitledBorder(
-			"Lumen Processing"));
+		bottomRightPanel.setBorder(BorderFactory.createTitledBorder("Lumen Processing"));
 		bottomRightPanel.add(btnLumen);
-		
+
 		rightPanel.setLayout(new MigLayout());
 		rightPanel.add(upRightPanel, "wrap, gapy 10::50, aligny top");
 		rightPanel.add(middlePanel, "aligny center, wrap, gapy 10::50");
 		rightPanel.add(bottomRightPanel);
-		
-		
+
 		leftPanel.setLayout(new MigLayout());
 		canvas.setMaximumSize(new Dimension(1024, 1024));
-		canvas.setMinimumSize(new Dimension(500, 500));	
+		canvas.setMinimumSize(new Dimension(500, 500));
 		Color newColor = new Color(200, 200, 255);
 		sliceSelector.setBackground(newColor);
 		leftPanel.add(canvas, "wrap");
-		leftPanel.add(sliceSelector, "growx");;
+		leftPanel.add(sliceSelector, "growx");
+		;
 
 		processingFrame.setLayout(new MigLayout());
 		processingFrame.add(leftPanel);
 		processingFrame.add(rightPanel, "east");
-		//processingFrame.setMinimumSize(new Dimension(1024, 1024));
+		// processingFrame.setMinimumSize(new Dimension(1024, 1024));
 		processingFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		processingFrame.pack();
 		processingFrame.setVisible(true);
@@ -208,10 +201,10 @@ public class PostProcessingWindow extends ImageWindow implements
 
 		btnInsert = new JButton("Modify Cell");
 		btnInsert.addActionListener(this);
-		
+
 		btnSave = new JButton("Save Results");
 		btnSave.addActionListener(this);
-		
+
 		btnLumen = new JButton("Add Lumen");
 
 		canvas.addComponentListener(new ComponentAdapter() {
@@ -222,13 +215,13 @@ public class PostProcessingWindow extends ImageWindow implements
 			}
 
 		});
-		
+
 		sliceSelector.addAdjustmentListener(new AdjustmentListener() {
 			public void adjustmentValueChanged(AdjustmentEvent e) {
 				int z = sliceSelector.getValue();
-				imp.setSlice(z);			
+				imp.setSlice(z);
 				updateOverlay();
-				
+
 			}
 		});
 	}
@@ -248,15 +241,16 @@ public class PostProcessingWindow extends ImageWindow implements
 
 		if (e.getSource() == btnInsert) {
 			this.addROI();
-			newCell.removeOverlappingRegions(all3dCells, polyRoi, canvas.getImage().getCurrentSlice(), all3dCells.get((Integer) cellSpinner.getValue() - 1).id_Cell);
+			newCell.removeOverlappingRegions(all3dCells, polyRoi, canvas.getImage().getCurrentSlice(),
+					all3dCells.get((Integer) cellSpinner.getValue() - 1).id_Cell);
 
 			checkOverlay.setSelectedIndex(1);
 			updateOverlay();
 		}
-		
+
 		if (e.getSource() == btnSave) {
 			this.savePlyFile(all3dCells, initialDirectory);
-			
+
 		}
 
 	}
@@ -266,19 +260,17 @@ public class PostProcessingWindow extends ImageWindow implements
 	 */
 	private void updateOverlay() {
 		if (checkOverlay.getSelectedItem() == "All overlays") {
-			
-			canvas.setOverlay(getOverlay(((Integer) cellSpinner
-				.getValue() - 1), canvas.getImage().getCurrentSlice(), all3dCells,
-				canvas.getImage(), true));
-			
+
+			canvas.setOverlay(getOverlay(((Integer) cellSpinner.getValue() - 1), canvas.getImage().getCurrentSlice(),
+					all3dCells, canvas.getImage(), true));
+
 			canvas.addOverlay(overlayResult);
-			
+
 		} else if (checkOverlay.getSelectedItem() == "Cell overlay") {
-				canvas.setOverlay(getOverlay(((Integer) cellSpinner
-					.getValue() - 1), canvas.getImage().getCurrentSlice(), all3dCells,
-					canvas.getImage(), false));
+			canvas.setOverlay(getOverlay(((Integer) cellSpinner.getValue() - 1), canvas.getImage().getCurrentSlice(),
+					all3dCells, canvas.getImage(), false));
 		}
-		
+
 		canvas.setImageOverlay(overlayResult);
 		overlayResult.setImage(canvas.getImage().getProcessor());
 		canvas.setImageUpdated();
@@ -302,49 +294,53 @@ public class PostProcessingWindow extends ImageWindow implements
 			int[] xPoly = ((PolygonRoi) r).getXCoordinates();
 			int[] yPoly = ((PolygonRoi) r).getYCoordinates();
 			int i = 0;
-				while (xPoly[i] != 0  | xPoly[i+1] != 0) {
-					i++;
-				}
-				polyRoi = new PolygonRoi(Arrays.copyOfRange(xPoly, 0, i-1), Arrays.copyOfRange(yPoly, 0, i-1), i-1, 2);
-				polyRoi.setLocation(r.getXBase(), r.getYBase());
+			while (xPoly[i] != 0 | xPoly[i + 1] != 0) {
+				i++;
+			}
+			polyRoi = new PolygonRoi(Arrays.copyOfRange(xPoly, 0, i - 1), Arrays.copyOfRange(yPoly, 0, i - 1), i - 1,
+					2);
+			polyRoi.setLocation(r.getXBase(), r.getYBase());
 		}
 		this.getImagePlus().deleteRoi();
-		
-	}
-	
-	static public void savePlyFile(ArrayList<Cell3D> allCells, String path_in) {
-		    if (!path_in.endsWith(File.separator)) {path_in=path_in+File.separator;}
-		    String path = path_in;
-		    	File dir = new File(path);
-		    	if (!dir.isDirectory()) {
-		    		System.out.println("Erreur, given path is not a directory");
-		    		return;
-		    	}
-		    	// By default removes all files in the folder
-		    	// But ask for confirmation if the folder is not empty...
-		    	if (dir.listFiles().length!=0) {
-		    		System.out.println("Saving will remove the content of the folder "+path+" that contains "+dir.listFiles().length+" files and folders.");
-		    	}
 
-		        // create the optimizer parameter element
-		        allCells.forEach(c->{
-		        	// Now writes all ply files for CellT object
-		        	for (int i = 0; i < c.dotsList.size(); i++) {
-		        		CellT cellt = new CellT(c, i);
-		        		String pathCell=path+File.separator+c.id_Cell+File.separator;
-		        	    File dirCell = new File(pathCell);		        	    
-		        	    // attempt to create the directory here
-		        	    if (dirCell.mkdir()) {
-		        	    	IOXmlPlyLimeSeg.saveCellTAsPly(cellt,pathCell+"T_"+cellt.frame+".ply");
-		        	    } else {
-		        	    	if (dirCell.exists()) {
-		        	    		IOXmlPlyLimeSeg.saveCellTAsPly(cellt,pathCell+"T_"+cellt.frame+".ply");
-		        	    	}
-		        	    }
-		        	}
-		        });
 	}
-	
+
+	static public void savePlyFile(ArrayList<Cell3D> allCells, String path_in) {
+		if (!path_in.endsWith(File.separator)) {
+			path_in = path_in + File.separator;
+		}
+		String path = path_in;
+		File dir = new File(path);
+		if (!dir.isDirectory()) {
+			System.out.println("Erreur, given path is not a directory");
+			return;
+		}
+		// By default removes all files in the folder
+		// But ask for confirmation if the folder is not empty...
+		if (dir.listFiles().length != 0) {
+			System.out.println("Saving will remove the content of the folder " + path + " that contains "
+					+ dir.listFiles().length + " files and folders.");
+		}
+
+		// create the optimizer parameter element
+		allCells.forEach(c -> {
+			// Now writes all ply files for CellT object
+			for (int i = 0; i < c.dotsList.size(); i++) {
+				CellT cellt = new CellT(c, i);
+				String pathCell = path + File.separator + c.id_Cell + File.separator;
+				File dirCell = new File(pathCell);
+				// attempt to create the directory here
+				if (dirCell.mkdir()) {
+					IOXmlPlyLimeSeg.saveCellTAsPly(cellt, pathCell + "T_" + cellt.frame + ".ply");
+				} else {
+					if (dirCell.exists()) {
+						IOXmlPlyLimeSeg.saveCellTAsPly(cellt, pathCell + "T_" + cellt.frame + ".ply");
+					}
+				}
+			}
+		});
+	}
+
 	/**
 	 * 
 	 * @param id
@@ -354,14 +350,13 @@ public class PostProcessingWindow extends ImageWindow implements
 	 * @param allOverlays
 	 * @return
 	 */
-	public Overlay getOverlay(Integer id, Integer frame,
-			ArrayList<Cell3D> cells, ImagePlus workingImP, boolean allOverlays)
-		{
-			Overlay ov = new Overlay();
-			if (workingImP != null) {
-				PointRoi roi;
-				if (allOverlays) {
-					
+	public Overlay getOverlay(Integer id, Integer frame, ArrayList<Cell3D> cells, ImagePlus workingImP,
+			boolean allOverlays) {
+		Overlay ov = new Overlay();
+		if (workingImP != null) {
+			PointRoi roi;
+			if (allOverlays) {
+
 				for (int nCell = 0; nCell < cells.size(); nCell++) {
 					ArrayList<DotN> dots = cells.get(nCell).getCell3DAt(frame);
 					Iterator<DotN> i = dots.iterator();
@@ -375,22 +370,21 @@ public class PostProcessingWindow extends ImageWindow implements
 							roi.setStrokeColor(colorCurrentCell);
 						}
 						ov.addElement(roi);
-					}	
-				}
-			}
-				else {
-					ArrayList<DotN> dots = cells.get(id).getCell3DAt(frame);
-					Iterator<DotN> i = dots.iterator();
-					while (i.hasNext()) {
-						DotN loadedDots = i.next();
-						roi = new PointRoi(loadedDots.pos.x, loadedDots.pos.y);
-						Color colorCurrentCell = new Color(255, 0, 0);
-						roi.setFillColor(colorCurrentCell);
-						ov.addElement(roi);
 					}
 				}
+			} else {
+				ArrayList<DotN> dots = cells.get(id).getCell3DAt(frame);
+				Iterator<DotN> i = dots.iterator();
+				while (i.hasNext()) {
+					DotN loadedDots = i.next();
+					roi = new PointRoi(loadedDots.pos.x, loadedDots.pos.y);
+					Color colorCurrentCell = new Color(255, 0, 0);
+					roi.setFillColor(colorCurrentCell);
+					ov.addElement(roi);
+				}
 			}
-			return ov;
 		}
+		return ov;
+	}
 
 }
